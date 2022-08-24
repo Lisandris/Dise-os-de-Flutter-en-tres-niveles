f4b11ac454c32e5183282adb0d69f8a1452bb3dd
Lisandris <lis170694@gmail.com>
2022-08-24 23:39:53 +0200

    Codigo Final optimizado

--- lib/screens/card_table.dart
+++ lib/screens/card_table.dart
@@ -68,30 +68,48 @@ class _SingleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+      return _CardBackground(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: color,
+            child: Icon( icon, size: 35, color: Colors.white),
+            radius: 30,
+          ),
+          const SizedBox( height: 10),
+          Text(text, style: TextStyle (color: this.color, fontSize: 18))
+        ],
+      )
+    );
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+
+  final Widget child;
+
+  const _CardBackground({
+    Key? key,
+    required this.child
+    }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-       margin: const EdgeInsets.all(15),
-       child: ClipRRect(
+      margin: const EdgeInsets.all(15),
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
         child: BackdropFilter(
           filter: ImageFilter.blur( sigmaX: 5, sigmaY: 5 ),
           child: Container(
             height: 180,
-            decoration: BoxDecoration( 
+           decoration: BoxDecoration( 
               color: const Color.fromRGBO(62, 66, 107, 0.7),
               borderRadius: BorderRadius.circular(20),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: color,
-                  child: Icon( icon, size: 35, color: Colors.white),
-                  radius: 30,
-                ),
-                SizedBox( height: 10),
-                Text(text, style: TextStyle (color: this.color, fontSize: 18))
-              ]
-            )
+            child: child, 
           ),
         ),
       ),
